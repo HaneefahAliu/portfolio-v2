c650e7847d9d37a5fe2bf92b1328150f2639f09c
Haneefah Aliu <aliuhaneefah@gmail.com>
2026-03-16 17:55:54 +0000

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -90,8 +90,8 @@ export default function Header() {
                     {/* Menu Footer */}
                     <div className="p-4 sm:p-6 lg:p-8">
                         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-4 sm:space-y-0">
-                            <div className="text-white font-inter font-light text-[12px] uppercase flex flex-row justify-between">
-                                <a href="mailto:aliuhaneefah@gmail.com" className=" underline decoration-solid hover:text-secondary-text transition-colors duration-200">
+                            <div className="text-white font-inter font-light text-[12px] uppercase">
+                                <a href="mailto:aliuhaneefah@gmail.com" className=" underline decoration-solid hover:text-secondary-text transition-colors duration-200 px-3 py-1">
                                     Email Me
                                 </a>
                                 <a href="https://drive.google.com/file/d/12BHwm1T7urf-gQgDPAoH4sIyc3YSSmgq/view?usp=sharing" target="_blank" className="hover:text-secondary-text transition-colors duration-200">
