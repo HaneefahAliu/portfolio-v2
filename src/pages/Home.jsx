8ce98a1a58475798647800838876e924601aada1
Haneefah Aliu <aliuhaneefah@gmail.com>
2026-04-08 08:08:41 -0400

    home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,31 @@ function Home() {
     return (
         <main className='page-grid'>
             <div className="mx-auto max-w-[1440px]">
+                 <div className="relative z-10 flex items-center min-h-[80vh] px-6 lg:px-10">
+    
+    <div className="relative w-full">
+      
+      {/* BIG TEXT */}
+      <h1 className="font-serif text-[64px] sm:text-[100px] lg:text-[180px] leading-[0.95] tracking-[-0.04em]">
+        Product
+        <br />
+        Designer.
+      </h1>
+
+      {/* SMALL FLOATING TEXT (LEFT) */}
+      <p className="absolute left-0 top-[60%] max-w-[220px] text-[12px] sm:text-[14px] text-black/50 leading-relaxed">
+        I care about how things look, how they work, and how they make people feel, so I design products people genuinely enjoy using.
+      </p>
+
+      {/* SMALL FLOATING TEXT (RIGHT) */}
+      <p className="absolute right-[10%] top-[35%] max-w-[200px] text-[12px] sm:text-[14px] text-black/50 leading-relaxed">
+        Hey! I'm Haneefah. A product designer based in Canada.
+      </p>
+
+    </div>
+  </div>
                 {/* Landing */}
-                <div className="min-h-screen pt-12 flex flex-col">
+                {/* <div className="min-h-screen pt-12 flex flex-col">
                     <section className="max-w-[520px]">
                         <AvailabilityPill />
                         <p className="mt-2 text-[14px] leading-[1.9] text-black/80 font-inter font-light">
@@ -28,7 +51,7 @@ function Home() {
                             <span className="block pl-[160px]">DESIGNER.</span>
                         </h1>
                     </section>
-                </div>
+                </div> */}
 
                 <div className='pt-32'>
                     {/* Projects */}
